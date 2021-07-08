7a31bba3609168f0891836ce0c7f622e8375e7c9
Stanislaw Baranski <stan@stan.bar>
2021-07-09 01:39:15 +0200

    feat: add authenticated paths

--- src/Pages/User.elm
+++ src/Pages/User.elm
@@ -1,9 +1,17 @@
-module Pages.User exposing (view)
+module Pages.User exposing (page)
 
 import View exposing (View)
+import Page exposing (Page)
+import Request exposing (Request)
+import Shared
+import Auth
 
+page : Shared.Model -> Request -> Page
+page shared _ =
+  Page.protected.static
+  (\user -> { view = view user })
 
-view : View msg
-view =
+view : Auth.User -> View msg
+view user =
     View.placeholder "User"
 
